c2c7c408eee6642a51709ded17ef36a516b8081a
BigFatStar <799029218@qq.com>
2020-12-02 10:14:52 +0800

    d

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,89 +1,90 @@
 <template>
   <div>
-    <!-- 这是一个导航栏 -->
-    <v-app-bar style=" position: fixed; z-index:4">
-      <div></div>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        <v-btn v-on:click="gopagehome" text>
-          {{ $t("lang.home.homeOverview") }}
-          <!-- 首页概括 -->
-        </v-btn>
-      </v-toolbar-title>
+    <!-- 这是一个轮播图 -->
+    <v-carousel hide-delimiters cycle>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
 
-      <v-toolbar-title>
-        <v-btn v-on:click="gopagehomepage" text>
-          {{ $t("lang.home.newCenter") }}
-          <!-- 新闻中心 -->
-        </v-btn>
-      </v-toolbar-title>
-      <v-toolbar-title>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark v-bind="attrs" v-on="on">
-              {{ $t("lang.home.newCenter") }}
-              <!-- 新闻中心 -->
-            </v-btn>
-          </template>
-          <v-card>
-            <a
-              ><v-list-item>
-                <v-list-item-title link>
-                  <v-btn>123</v-btn>
-                </v-list-item-title>
-              </v-list-item>
-            </a>
-            <a
-              ><v-list-item>
-                <v-list-item-title link>
-                  <v-btn>123</v-btn>
-                </v-list-item-title>
-              </v-list-item>
-            </a>
-          </v-card>
-        </v-menu>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn v-on:click="changeLangEvent" text>
-        {{ $t("lang.home.cutLanguage") }}
-        <!-- 切换语言 -->
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <!-- 一段文字介绍 -->
+    <div class="bannerSlogan">
+      <div>
+        <div class="enSlogan">
+          了解我们的产品
+        </div>
+        <div class="cnSlogan">
+          专业专注，提供涵盖测绘地理信息全产业链的产品和服务
+        </div>
+      </div>
+    </div>
 
-    <router-view></router-view>
+   <!-- 我们的产品 -->
+    <vm-deviceCard :devicemsg="devicemsg" ></vm-deviceCard>
+    
   </div>
 </template>
 <script>
-import router from "@/router";
+/**
+ * 这个是网站的首页
+ */
+import deviceCard from "@/page/home/library/deviceCard.vue";
 export default {
-  
-  name: "home",
+  components: {
+    "vm-deviceCard": deviceCard,
+  },
+  name: "homepage",
   data() {
-    return {};
+    return {
+      
+      devicemsg:{
+        url:'http',
+      },
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
   },
   methods: {
-    /**
-     * 去首页的方法
-     */
-    gopagehomepage() {
-      router.push("newHomePage");
-    },
-    gopagehome() {
-      router.push("homepage");
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     },
 
-    changeLangEvent() {
-      if (this.$i18n.locale === "zh-CN") {
-        this.$i18n.locale = "en-US"; //关键语句
-        console.log("en-US");
-      } else {
-        this.$i18n.locale = "zh-CN"; //关键语句
-        console.log("zh-CN");
-      }
-    },
+    
   },
 };
 </script>
+
+<style>
+.enSlogan {
+  margin: auto;
+  display: block;
+  font-size: 50px;
+}
+.bannerSlogan {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding-top: 20px;
+  text-align: center;
+  color: gray;
+  pointer-events: none;
+}
+.cnSlogan {
+  font-size: 25px;
+}
+</style>
