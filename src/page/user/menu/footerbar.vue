a5319225e4f30d18e3c4f765ac767072558846a9
jiangjiang <1789799647@qq.com>
2020-12-07 12:58:05 +0800

    d

---
+++ src/page/user/menu/footerbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 底部导航栏 -->
+<v-footer
+      app
+      color="transparent"
+      height="72"
+      inset
+    >
+      <v-text-field
+        background-color="grey lighten-1"
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+      ></v-text-field>
+    </v-footer>
+</template>
+
+<script lang="ts">
+export default({
+  name: "footerbar",
+});
+</script>
