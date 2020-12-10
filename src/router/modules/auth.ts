8b49822d5e0c1b202a4b7419285fceb1e452ea90
jiangjiang <1789799647@qq.com>
2020-12-10 16:56:12 +0800

    d

--- src/router/modules/auth.ts
+++ src/router/modules/auth.ts
@@ -2,10 +2,6 @@ import auth from "@/page/auth/login.vue";
 
 export default {
     path: "/auth",
-<<<<<<< HEAD
-    name: 'login',
-=======
     name: 'auth',
->>>>>>> e
     component: auth,   
 };
