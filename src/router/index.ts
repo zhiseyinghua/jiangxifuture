ceaabe1fa9f02c7639b5547023b24cbfbb9b78c4
jiangjiang <1789799647@qq.com>
2020-12-10 17:10:28 +0800

    d

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,14 @@ import Router from "vue-router";
 import home from "./modules/home";
 import news from "./modules/news";
 import auth from "./modules/auth";
+import user from './modules/user';
 Vue.use(Router);
 // 这是一个路由管理的模块，是各个路由的出口
 export default new Router({
   routes: [
     auth,
     home,
-    news
-   
+    news,
+    user
   ]
 });
