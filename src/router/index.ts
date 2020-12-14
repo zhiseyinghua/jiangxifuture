48537b2c7602e675c993be58ed37b966db531fcd
jiangjiang <1789799647@qq.com>
2020-12-14 17:10:22 +0800

    ：d

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,14 @@ import Router from "vue-router";
 import home from "./modules/home";
 import news from "./modules/news";
 import auth from "./modules/auth";
-<<<<<<< HEAD
 import login from "./modules/login";
-=======
 import Auth from "@/page/auth/signin.vue";
-import user from './modules/user';
->>>>>>> 0aa5bb864d24a03c01259c5796c31cd7ffabe75c
 import solution from "./modules/solution";
 import test from "./modules/test";
 Vue.use(Router);
 // 这是一个路由管理的模块，是各个路由的出口
 export default new Router({
   routes: [
-<<<<<<< HEAD
     home,
     news,
     auth,
@@ -29,16 +24,12 @@ export default new Router({
     {
       path:"*",
       redirect:home
-    }
-=======
-    {
-      path:"/",
-      component:Auth
     },
-    auth,
-    home,
-    news,
-    user,
+    // {
+    //   path:"/",
+    //   component:Auth
+    // },
+
     solution,
     
     // {
@@ -49,6 +40,5 @@ export default new Router({
     //   path:"*",
     //   redirect:home
     // }
->>>>>>> 0aa5bb864d24a03c01259c5796c31cd7ffabe75c
   ]
 });
