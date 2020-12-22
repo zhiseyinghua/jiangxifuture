f6e8a0b6ef99c857e37e0ab0feb37d98a1536b46
huangwenqiang <huangwenqiang@chain-c.com.com>
2020-12-22 09:54:42 +0800

    d

--- vue.config.js
+++ vue.config.js
@@ -10,14 +10,8 @@ module.exports = {
       https: true,
       hotOnly: false, 
       proxy: {
-          '/api': {
-              target: 'http://localhost:3000', //API服务器的地址
-              ws: true,  //代理websockets
-              changeOrigin: true, // 虚拟的站点需要更管origin
-              pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-                  '^/api': '/'
-              }
-          }
+           
       },
-    }
+    },
+    
 };
