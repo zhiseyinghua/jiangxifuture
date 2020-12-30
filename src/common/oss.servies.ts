82b682ee6c6b0cde29cb8668e8b023ab7fac5596
huangwenqiang <huangwenqiang@chain-c.com.com>
2020-12-30 11:31:44 +0800

    d

--- src/common/oss.servies.ts
+++ src/common/oss.servies.ts
@@ -1,6 +1,12 @@
 import oss from 'ali-oss'
+import authServies from '@/page/auth/auth.servies'
+import { switchMap } from 'rxjs/operators'
 export default class OSSServies{
     public static putfileToAliyunS3(){
-        
+        authServies.getS3authority().pipe(
+            // switchMap(data=>{
+
+            // })
+        )
     }
 }
