471b36f925bf51aaa02894e4f6b19491f7616aa7
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-05 16:52:04 +0800

    d

--- src/common/oss.servies.ts
+++ src/common/oss.servies.ts
@@ -5,8 +5,6 @@ import { AccessS3Token } from "./common.interface";
 import { AliyunConfig } from "@/page/auth/aliyun.common";
 var OSS = require("ali-oss");
 
-
-
 /**
  * 这是一个关于阿里云s3文件存储桶的服务
  */
@@ -18,15 +16,23 @@ export default class OSSServies {
    * @param bukcket 上传到oss的那个桶中
    * @param isPubilc  是否公开(生成的文件url是否完全公开)，传入 string 'public' 则表明这个文件完全公开
    */
-  public static putfileToAliyunS3(file: any,bukcket:string,isPubilc?:string): Observable<any> {
-    let bucket = AliyunConfig.s3bucket
-    if(isPubilc && isPubilc == 'public'){
-      bucket = AliyunConfig.s3bucketPubilc
+  public static putfileToAliyunS3(
+    file: any,
+    bukcket: string,
+    isPubilc?: string
+  ): Observable<any> {
+    let bucket = AliyunConfig.s3bucket;
+    if (isPubilc && isPubilc == "public") {
+      bucket = AliyunConfig.s3bucketPubilc;
     }
     return authServies.getS3authority().pipe(
       switchMap((data) => {
         let _s3Token = data as AccessS3Token;
-        console.log(this.log + "putfileToAliyunS3 data", _s3Token.AccessKeyId,_s3Token.AccessKeySecret);
+        console.log(
+          this.log + "putfileToAliyunS3 data",
+          _s3Token.AccessKeyId,
+          _s3Token.AccessKeySecret
+        );
         let client = new OSS({
           bucket: bucket,
           // region以杭州为例（oss-cn-hangzhou），其他region按实际情况填写。
@@ -36,25 +42,21 @@ export default class OSSServies {
           accessKeySecret: _s3Token.AccessKeySecret,
           stsToken: _s3Token.SecurityToken,
         });
-        let name = OSSServies.fuwenbenfileName(file)  
-        return client.put(bukcket + '/'+name, file);
-      }),
+        let name = OSSServies.fuwenbenfileName(file);
+        return client.put(bukcket + "/" + name, file);
+      })
     );
   }
-
-
-
-
 
   public static fuwenbenfileName(file: any) {
     let suijiname = OSSServies.randomWord(false, 32);
     // let arr = file.type.split("/")[1];
     let arr = file.name.split(".");
-    console.log('111111111111111111111111111111',arr,file.name)
-    let name = arr[arr.length -1 ]
-    console.log(name) // jpeg
-    console.log('fuwenbenfileName arr',arr) // jpeg
-    var uuid =  suijiname + "." + name;
+    console.log("111111111111111111111111111111", arr, file.name);
+    let name = arr[arr.length - 1];
+    console.log(name); // jpeg
+    console.log("fuwenbenfileName arr", arr); // jpeg
+    var uuid = suijiname + "." + name;
     return uuid;
   }
 
