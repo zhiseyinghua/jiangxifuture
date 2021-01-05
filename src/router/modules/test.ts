f447d82772695a83ac09a49f045812eb8c919e15
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-05 17:16:41 +0800

    d

--- src/router/modules/test.ts
+++ src/router/modules/test.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import test from "@/page/test/hwq_test.vue";
-=======
-// import test from "@/page/test/test.vue";
->>>>>>> fa24231b1440d086a436e6b819d0db33f5ab8887
 
 export default {
     path: "/test",
     name: 'test',
-    component: ()=> import('@/page/test/test.vue'),
+    component: ()=> import('@/page/test/hwq_test.vue'),
 }
