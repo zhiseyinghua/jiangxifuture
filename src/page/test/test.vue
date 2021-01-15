a4d2665c1dc55c4788e39a598697032b31ced9ef
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-15 15:39:08 +0800

    d

--- src/page/test/test.vue
+++ src/page/test/test.vue
@@ -1,15 +1,21 @@
-
 <template>
   <div>
     <v-btn @click="goutouser">进入别人页面</v-btn>
-     <v-btn @click="goutouserziji">进入自己页面</v-btn>
+    <v-btn @click="goutouserziji">进入自己页面</v-btn>
   </div>
 </template>
 <script>
 import axios from "axios";
+import authServies from "@/page/auth/auth.servies";
+import { switchMap } from "rxjs/operators";
+import { of } from "rxjs";
+import store from "@/store";
+import router from "@/router";
 export default {
   data: function() {
-    return {};
+    return {
+      companyname: "",
+    };
   },
   created: function() {
     const authorization = localStorage.getItem("token");
@@ -30,18 +36,44 @@ export default {
     goutouser() {
       console.log("进入user");
       this.$router.push({
-        path: "user",
+        path: "user/account",
         query: { hash: "1", range: "1", index: "1" },
       });
     },
     goutouserziji() {
       console.log("进入user");
       this.$router.push({
-        path: "user",
-        query: { "hash": "auth-2020-12-24",
-  "range": "682c4b8f-2749-4d65-aaa1-488e7f53d403",
-  "index": "682c4b8f-2749-4d65-aaa1-488e7f53d403", },
+        path: "user/account",
+        query: {
+          hash: "auth-2020-12-24",
+          range: "682c4b8f-2749-4d65-aaa1-488e7f53d403",
+          index: "682c4b8f-2749-4d65-aaa1-488e7f53d403",
+        },
       });
+    },
+    upuserdata() {
+      var authkeyToken = store.state.login.idtoken;
+      authServies
+        .asyncjiexiJwtDecjeck(authkeyToken)
+        // .pipe(
+        //   switchMap((data) => {
+        //     return accountserves.byuseridgetUserDate({
+        //       hash: data.hash,
+        //       range: data.range,
+        //       index: data.index,
+        //     });
+        //   })
+        // )
+        .subscribe((data) => {
+          // this.useritems = [
+          //   { name: "姓名", data: data.usernickname },
+          //   { name: "手机", data: data.telephone },
+          //   { name: "邮箱", data: data.usermail },
+          //   { name: "位置", data: data.position },
+          //   { name: "公司", data: data.companyname },
+          // ];
+          console.log(this.companyname);
+        });
     },
   },
 };
--- src/page/user/account/account.vue
+++ src/page/user/account/account.vue
@@ -3,8 +3,7 @@
     <!-- 页面排版 -->
     <v-row no-gutters>
       <v-col cols="auto"></v-col>
-      <v-col md="6" sm="8"
-      cols="11" offset-md="3" offset-sm="2">
+      <v-col md="6" sm="8" cols="11" offset-md="3" offset-sm="2">
         <!-- 信息框 -->
         <v-card>
           <v-card-actions>
@@ -13,7 +12,7 @@
             <!-- 修改框弹窗 -->
             <v-dialog v-model="dialog" width="500">
               <template v-slot:activator="{ on, attrs }">
-                <v-btn icon v-bind="attrs" v-on="on" @click="editbtn">
+                <v-btn icon v-bind="attrs" v-on="on">
                   <v-icon>mdi-account-edit-outline</v-icon>
                 </v-btn>
               </template>
@@ -21,29 +20,19 @@
                 <v-card-title class="headline grey lighten-5">
                   修改基本信息</v-card-title
                 >
-                 <v-card class="ma-10" flat>  
-                  <!-- <v-text-field
-                    v-for="item in items"
-                  :key="item"
-                    :label="item"
-                    outlined
-                    clearable
-                    dense
-                  >
-                  </v-text-field>  -->
-                  <v-text-field
-                    v-for="item in userInfo"
-                    :key="item.name"
-                    :label="item.name"
-                    :value="item.data"
-                    outlined
-                    clearable
-                    dense
-                  >
-                  {{item.data}}
-                  </v-text-field>
-                 </v-card>
-                 <v-divider></v-divider>
+                <v-card class="ma-10" flat>
+                  <div v-for="(user, i) in useritems" :key="i">
+                    <v-text-field
+                      :label="user.name"
+                      :value="user.data"
+                      outlined
+                      clearable
+                      dense
+                    >
+                    </v-text-field>
+                  </div>
+                </v-card>
+                <v-divider></v-divider>
                 <v-card-actions>
                   <v-btn color="primary" text @click="dialog = false">
                     暂不修改
@@ -53,7 +42,7 @@
                     确认修改
                   </v-btn>
                 </v-card-actions>
-                </v-card>
+              </v-card>
             </v-dialog>
           </v-card-actions>
           <v-divider class="mx-6"></v-divider>
@@ -69,7 +58,7 @@
           </v-card-actions>
           <v-row class="mx-16 mt-1">
             <v-col class="d-flex mt-2 pt-0" cols="8">
-              <v-card-text> 姓名 </v-card-text>
+              <v-card-text> 昵称 </v-card-text>
               <v-card-text> {{ usernickname }} </v-card-text>
             </v-col>
           </v-row>
@@ -107,7 +96,7 @@
       </v-col>
     </v-row>
   </v-container>
-</template> 
+</template>
 
 <script lang="ts">
 import authServies from "@/page/auth/auth.servies";
@@ -115,6 +104,9 @@ import { switchMap } from "rxjs/operators";
 import { of } from "rxjs";
 import accountserves from "./account.serve";
 import { BaceUserdata, EditUserData } from "../user.interface";
+import store from "@/store";
+// import router from "@/router";
+
 export default {
   data() {
     return {
@@ -126,69 +118,83 @@ export default {
       position: "",
       startdate: "",
       companyname: "",
-      dialog: true,
+      dialog: false,
+      useritems: [{}],
     };
   },
-  computed: {
-    userInfo: function () { 
-      let _this = this;
-      const items =  [
-       {name:"姓名",data:_this.usernickname}, 
-       {name:"手机",data:_this.telephone},
-       {name:"邮箱",data:_this.usermail},
-       {name:"位置",data:_this.position},
-       {name:"公司",data:_this.companyname},
-       ]
-      return items;
-      console.warn(this.usernickname)
+  methods: {
+    upuserdata() {
+      var authkeyToken = store.state.login.idtoken;
+      authServies
+        .asyncjiexiJwtDecjeck(authkeyToken)
+        // .pipe(
+        //   switchMap((data) => {
+        //     return accountserves.byuseridgetUserDate({
+        //       hash: data.hash,
+        //       range: data.range,
+        //       index: data.index,
+        //     });
+        //   })
+        // )
+        .subscribe((data) => {
+          // this.useritems = [
+          //   { name: "姓名", data: data.usernickname },
+          //   { name: "手机", data: data.telephone },
+          //   { name: "邮箱", data: data.usermail },
+          //   { name: "位置", data: data.position },
+          //   { name: "公司", data: data.companyname },
+          // ];
+          
+          console.log(this.companyname);
+        });
     }
   },
-  methods: {
-    editbtn() {
-      let userdata: EditUserData = {
-        usernickname: "",
-        usermail: "",
-        userico: "",
-        telephone: "",
-        position: "",
-        startdate: "",
-        companyname: "",
-      };
-    },
-  },
+
   created() {
-    // var authkeyToken = this.$store.state.login.idtoken;
+    // if (this.$route.path === "/user") {
+    //   this.changeShow();
+    // }
+    // console.log(this['$route'].query);
+    // let authdata = authServies.jiexiJwtDecjeck(store.state.login.idtoken);
+    // console.log("11111111111111", this, router.app.$route.query);
+    // let storeRange = authdata.range;
+    // let routeRange = router.app.$route.query;
+    // console.log("1111111111111", storeRange, routeRange);
+    // if (storeRange == routeRange) {
+    //   console.log("account.vue 进入了自己的user页面");
+    // } else if (storeRange !== routeRange) {
+    //   console.log("account.vue 进入别的user主页");
+    // } else {
+    // }
+    var authkeyToken = store.state.login.idtoken;
     // authServies
     //   .asyncjiexiJwtDecjeck(authkeyToken)
     //   .pipe(
-    //     // switchMap((data) => {
-    //     //   // console.log(data);
-
-    //     //   this.uuid = data.range;
-
-    //     //   // TODO:
-    //     //   // return of('11')
-    //     //   return accountserves.byuseridgetUserDate({
-    //     //     hash: data.hash,
-    //     //     range: data.range,
-    //     //     index: data.index,
-    //     //   });
-    //     // })
+    //     switchMap((data) => {
+    //       return accountserves.byuseridgetUserDate({
+    //         hash: data.hash,
+    //         range: data.range,
+    //         index: data.index,
+    //       });
+    //     })
     //   )
     //   .subscribe((data) => {
-    //     // this.usernickname = data.usernickname;
-    //     // this.telephone = data.telephone;
-    //     // this.usermail = data.usermail;
-    //     // this.userico = data.userico;
-    //     // this.position = data.position;
-    //     // this.startdate = data.startdate;
-    //     // this.companyname = data.companyname;
-    //     // let _data = data as BaceUserdata;
-    //     // // this.name = _data.usernickname;
-    //     // console.log(data);
-    //     // console.warn("消息内容！");
+    //     this.useritems = [
+    //       { name: "姓名", data: data.usernickname },
+    //       { name: "手机", data: data.telephone },
+    //       { name: "邮箱", data: data.usermail },
+    //       { name: "位置", data: data.position },
+    //       { name: "公司", data: data.companyname },
+    //     ];
+
+    //     this.usernickname = data.usernickname;
+    //     this.telephone = data.telephone;
+    //     this.usermail = data.usermail;
+    //     this.userico = data.userico;
+    //     this.position = data.position;
+    //     this.startdate = data.startdate;
+    //     this.companyname = data.companyname;
     //   });
-    // // console.warn(authkeyToken);
   },
 };
 </script>
