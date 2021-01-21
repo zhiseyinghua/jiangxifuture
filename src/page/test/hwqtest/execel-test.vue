e96983cf4195fa51f3be2bb8b6c9b7f1f388129b
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-21 14:29:01 +0800

    d

---
+++ src/page/test/hwqtest/execel-test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="height:600px" class="amap-page-container">
+    <el-amap
+      vid="amapDemo"
+      :zoom="zoom"
+      class="amap-demo"
+      :events="events"
+    >
+    </el-amap>
+    <div class="toolbar">
+      position: [{{ lng }}, {{ lat }}] address: {{ address }}
+    </div>
+  </div>
+</template>
+<script>
+    module.exports = {
+      data: function() {
+        return {
+          
+        };
+      }
+    };
+</script>
+
+<style>
+.amap-demo {
+  height: 300px;
+}
+</style>
