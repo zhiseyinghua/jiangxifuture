abd70c0f31632401e16c8d9c28da7345f9ad8e8d
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-22 17:24:47 +0800

    d

--- src/page/task_systems/taskDetail/projectDetail.vue
+++ src/page/task_systems/taskDetail/projectDetail.vue
@@ -3,30 +3,75 @@
     <div class="font-weight-medium" style="font-size: 1.5em;">
       任务详情
     </div>
-    <v-divider class="mt-5"></v-divider>
-    <v-menu offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn text v-bind="attrs" v-on="on">
-          <v-icon large color="green darken-2">
-            mdi-alarm-check
-          </v-icon>
-          <v-icon large color="green darken-2">
-            mdi-alarm
-          </v-icon>
-          <v-icon large color="green darken-2">
-            mdi-alarm-off
-          </v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item>
-          <v-list-item-title>完成</v-list-item-title>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-title>完成</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <v-divider class="mt-5 mb-12"></v-divider>
+    <v-row>
+      <v-col>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text style="height:80px" v-bind="attrs" v-on="on">
+              <v-row>
+                <v-col>
+                  <v-icon v-show="false" color="green darken-2">
+                    mdi-alarm-check
+                  </v-icon>
+                  <v-icon v-show="false" color="green darken-2">
+                    mdi-alarm
+                  </v-icon>
+                  <v-icon v-show="true" size="70" color="green darken-2">
+                    mdi-alarm-off
+                  </v-icon>
+                </v-col>
+                <v-col class="mt-2">
+                  <div style="font-size: 1.5em;" class="grey--text mb-2">
+                    进行时
+                  </div>
+                  <p>当前状态</p>
+                </v-col>
+              </v-row>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="abs">
+              <v-list-item-title>
+                <v-icon>mdi-alarm-off</v-icon>
+                <span>完成</span>
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="abs">
+              <v-list-item-title>
+                <v-icon>mdi-alarm-off</v-icon>
+                <span>未完成</span>
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="abs">
+              <v-list-item-title>
+                <v-icon>mdi-alarm-off</v-icon>
+                <span>进行时</span>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+      <v-col>
+        <div>
+          <v-row>
+          <v-col>
+            <v-avatar size="70">
+              <img
+                alt="user"
+                src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3846290151,2766954484&fm=26&gp=0.jpg"
+              />
+            </v-avatar>
+          </v-col>
+          <v-col>
+            <p class="">
+              John Doe
+            </p>
+          </v-col>
+        </v-row>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -76,6 +121,9 @@ export default {
       },
     ],
   }),
+  methods: {
+    abs() {},
+  },
 };
 </script>
 
