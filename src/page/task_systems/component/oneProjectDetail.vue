fc682a2a2afaf30f2579a253d4d00535cfa20f51
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-02-19 20:15:13 +0800

    d

---
+++ src/page/task_systems/component/oneProjectDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-actions class="float-right my-2"
+      ><v-btn
+        to="/taskSystems/projectDetail"
+        text
+        class="light-blue--text text--accent-4"
+        >详情</v-btn
+      ></v-card-actions
+    >
+    <v-card-title class="text-center">项目名称：吴城测绘</v-card-title>
+    <v-card-subtitle> 甲方信息 </v-card-subtitle>
+    <v-card-text> 名字： 咚咚呱 </v-card-text>
+    <v-card-text> 电话： 17607924259 </v-card-text>
+    <v-card-subtitle>项目信息</v-card-subtitle>
+    <v-card-text> 地点： 吴城 </v-card-text>
+    <v-card-text> 面积： 200亩 </v-card-text>
+    <v-card-text> 类型： 不动产测绘 </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+    
+}
+</script>
