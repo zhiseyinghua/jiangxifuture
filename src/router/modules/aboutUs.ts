00f947c960b12435f5c46c278011112e11845136
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-02-19 10:12:48 +0800

    d

--- src/router/modules/aboutUs.ts
+++ src/router/modules/aboutUs.ts
@@ -1,7 +1,7 @@
 import aboutUs from "@/page/aboutUs/aboutUs.vue";
 
 export default {
-    path: '/aboutUs',
-    name:'aboutUs',
-    component: aboutUs
-}
+  path: "/aboutUs",
+  name: "aboutUs",
+  component: aboutUs,
+};
--- src/router/modules/task_systems.ts
+++ src/router/modules/task_systems.ts
@@ -1,20 +1,29 @@
 export default {
-    path: '/taskSystems',
-    name: 'taskSystems',
-    component: () => import('@/page/task_systems/index.vue'),
-    meta:{requireAuth:true},
-    children: [
-        {
-            path: "project", name:"project", component: () => import('@/page/task_systems/taskDetail/project.vue')
-        },
-        {
-            path:"progresses",name: "progresses", component: () => import('@/page/task_systems/taskDetail/progresses.vue')
-        },
-        {
-            path:"group", name: "group", component: () => import('@/page/task_systems/taskDetail/group.vue')
-        },
-        {
-            path:"projectDetail", name: "projectDetail", component: () => import('@/page/task_systems/taskDetail/projectDetail.vue')
-        }
-    ]
-}
+  path: "/taskSystems",
+  name: "taskSystems",
+  component: () => import("@/page/task_systems/index.vue"),
+  meta: { requireAuth: true },
+  children: [
+    {
+      path: "project",
+      name: "project",
+      component: () => import("@/page/task_systems/taskDetail/project.vue"),
+    },
+    {
+      path: "progresses",
+      name: "progresses",
+      component: () => import("@/page/task_systems/taskDetail/progresses.vue"),
+    },
+    {
+      path: "group",
+      name: "group",
+      component: () => import("@/page/task_systems/taskDetail/group.vue"),
+    },
+    {
+      path: "projectDetail",
+      name: "projectDetail",
+      component: () =>
+        import("@/page/task_systems/taskDetail/projectDetail.vue"),
+    },
+  ],
+};
