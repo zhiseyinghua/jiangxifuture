86fb9025296460a88b483997d02684fa31f49665
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-02-22 09:39:24 +0800

    d

--- src/page/task_systems/taskDetail/projectDetail.vue
+++ src/page/task_systems/taskDetail/projectDetail.vue
@@ -12,7 +12,7 @@
               <v-avatar size="55">
                 <img
                   alt="user"
-                  src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F01%2F12%2F50%2F1656f5fcc066ff0.jpg&refer=http%3A%2F%2Fbpic.588ku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616405735&t=3323fee2f6a9ffffbf0fdfef5cd8eff9"
+                  src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3723801689,3918778516&fm=26&gp=0.jpg"
                 />
               </v-avatar>
             </v-col>
