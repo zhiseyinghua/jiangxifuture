b8936b79a09e0554222a9d87eb5778fb1894703f
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-03-03 13:19:11 +0800

    d

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     proxy: {
       "/api": {
         // target: "http://127.0.0.1:3000/", //API服务器的地址
-        target: "http://134.175.220.57:3000/", //API服务器的地址
+        target: "http://134.175.220.57:80/", //API服务器的地址
         // ws: true, //代理websockets
         changeOrigin: true, // 虚拟的站点需要更管origin
         pathRewrite: {
